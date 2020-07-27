56da30a3b963b5e09afb21b51d25454963b7db4d
Kyle Bank <Kyle.Ryan.Banks@gmail.com>
2020-07-27 01:08:33 -0500

    refactor test structure

--- apps/chore-monster-e2e/src/integration/app.spec.ts
+++ apps/chore-monster-e2e/src/integration/app.spec.ts
@@ -1,19 +1,20 @@
-import { chmodSync } from 'fs';
-
 describe('chore-monster', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should redirect you to /heroes', () => {
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/heroes');
+  describe('/', () => {
+    it('should redirect to /heroes', () => {
+      cy.visit('/');
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/heroes');
+      });
     });
   });
 
-  it('should display welcome message', () => {
-    cy.get('h1').contains('Welcome to Chore Monster!');
-  });
+  describe('heroes', () => {
+    it('should display welcome message', () => {
+      cy.get('h1').contains('Welcome to Chore Monster!');
+    });
 
-  it('should display a select hero button', () => {
-    cy.get('[data-cy=select-hero]');
+    it('should display a select hero button', () => {
+      cy.get('[data-cy=select-hero]');
+    });
   });
 });
