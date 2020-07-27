1c075c5c48a0cac3ec1e8d1a17f642f6492cd60b
Kyle Bank <Kyle.Ryan.Banks@gmail.com>
2020-07-26 23:17:12 -0500

    change npmScope from chore-monster to chore. Add empty routing

--- apps/chore-monster/src/app/app.module.ts
+++ apps/chore-monster/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppRoutingModule],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
